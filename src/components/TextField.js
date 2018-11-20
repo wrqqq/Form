ab5933d5ec0dc39b356d3dc7f5749d8384bd40b8
Илья Солдатенков <wrqqqrrr@gmail.com>
2018-11-20 23:31:35 +0300

    Clean structure, add several packages

---
+++ src/components/TextField.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+});
+
+class TextFields extends React.Component {
+    state = {
+        name: 'Cat in the Hat',
+        age: '',
+        multiline: 'Controlled',
+        currency: 'EUR',
+    };
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form className={classes.container} noValidate autoComplete="off">
+                <TextField
+                    id="standard-name"
+                    label="Name"
+                    className={classes.textField}
+                    value={this.state.name}
+                    onChange={this.handleChange('name')}
+                    margin="normal"
+                />
+                <TextField
+                    id="standard-uncontrolled"
+                    label="Uncontrolled"
+                    defaultValue="foo"
+                    className={classes.textField}
+                    margin="normal"
+                />
+            </form>
+        );
+    }
+}
+
+export default withStyles(styles)(TextFields);
