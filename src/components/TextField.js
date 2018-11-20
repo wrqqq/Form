4e0c32b2f5016369b7839da9c06299db2b2f6658
Илья Солдатенков <wrqqqrrr@gmail.com>
2018-11-21 01:46:26 +0300

    Css structure, grid, button

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,13 +2,24 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import classNames from 'classnames';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
+const styles = () => ({
     textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 300,
+
+    },
+    container: {
+        padding: 30
+    },
+    form: {
+        paddingLeft: 20,
+        paddingRight: 20
+    },
+    button: {
+        width: 320,
+        display: 'block',
+        marginTop: 10
     }
 });
 
@@ -31,54 +42,71 @@ class TextFields extends React.Component {
         const { classes } = this.props;
 
         return (
-            <Grid container spacing={24}>
-                <form className={classes.container} autoComplete="off">
-                    <Grid item xs={12}>
-                        <TextField
-                            id="standard-name"
-                            label="Имя"
-                            className={classes.textField}
-                            value={this.state.name}
-                            onChange={this.handleChange('name')}
-                            margin="normal"
-                        />
-                        <TextField
-                            id="standard-surname"
-                            label="Фамилия"
-                            className={classes.textField}
-                            value={this.state.surname}
-                            onChange={this.handleChange('surname')}
-                            margin="normal"
-                        />
+                <form autoComplete="off" className={classes.form}>
+                    <Grid container spacing={24}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="standard-name"
+                                label="Имя"
+                                fullWidth
+                                className={classes.textField}
+                                value={this.state.name}
+                                onChange={this.handleChange('name')}
+                                margin="normal"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="standard-surname"
+                                label="Фамилия"
+                                fullWidth
+                                className={classes.textField}
+                                value={this.state.surname}
+                                onChange={this.handleChange('surname')}
+                                margin="normal"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="standard-surname"
+                                label="Почта"
+                                fullWidth
+                                className={classes.textField}
+                                value={this.state.mail}
+                                onChange={this.handleChange('surname')}
+                                margin="normal"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="standard-surname"
+                                label="Номер телефона"
+                                fullWidth
+                                className={classes.textField}
+                                value={this.state.phone}
+                                onChange={this.handleChange('phone')}
+                                margin="normal"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="standard-surname"
+                                label="Дополнительная информация"
+                                fullWidth
+                                multiline={true}
+                                rows={6}
+                                rowsMax={10}
+                                className={classes.textField}
+                                value={this.state.text}
+                                onChange={this.handleChange('text')}
+                                margin="normal"
+                            />
+                        </Grid>
                     </Grid>
-                    <TextField
-                        id="standard-surname"
-                        label="Почта"
-                        className={classes.textField}
-                        value={this.state.mail}
-                        onChange={this.handleChange('surname')}
-                        margin="normal"
-                    />
-                    <TextField
-                        id="standard-surname"
-                        label="Номер телефона"
-                        className={classes.textField}
-                        value={this.state.phone}
-                        onChange={this.handleChange('phone')}
-                        margin="normal"
-                    />
-
-                    <TextField
-                        id="standard-surname"
-                        label="Дополнительная информация"
-                        className={classes.textField}
-                        value={this.state.text}
-                        onChange={this.handleChange('text')}
-                        margin="normal"
-                    />
-
+                    <Button variant="contained" color="primary" size='large' className={classes.button}>
+                        Отправить
+                    </Button>
                 </form>
-            </Grid>
         );
     }
 }
