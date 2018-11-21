dfa7eb8e0794a7272f38c6552e4f59010af7e403
Илья Солдатенков <wrqqqrrr@gmail.com>
2018-11-21 03:47:46 +0300

    Refactor validating fields

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import TextFields from './components/TextField';
+import Form from './components/TextField';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Форма обратной связи:</h1>
-        <TextFields/>
+        <Form/>
       </div>
     );
   }
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import classNames from 'classnames';
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import Grid from '@material-ui/core/Grid';
+import InputMask from 'react-input-mask';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -34,67 +36,98 @@ class TextFields extends React.Component {
 
     handleChange = name => event => {
         this.setState({
-            [name]: event.target.value,
+            [name]: event.target.value
         });
     };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // get our form data out of state
+        const { fname, lname, email } = this.state;
+
+        axios.post('/', { fname, lname, email })
+            .then((result) => {
+                //access the results here....
+            });
+    }
 
     render() {
         const { classes } = this.props;
 
         return (
-                <form autoComplete="off" className={classes.form}>
+            <ValidatorForm
+                ref="form"
+                onSubmit={this.handleSubmit}
+                onError={errors => console.log(errors)}
+                className={classes.form}
+            >
                     <Grid container spacing={24}>
                         <Grid item xs={12} sm={6}>
-                            <TextField
+                            <TextValidator
                                 id="standard-name"
                                 label="Имя"
+                                name='name'
                                 fullWidth
                                 className={classes.textField}
                                 value={this.state.name}
                                 onChange={this.handleChange('name')}
                                 margin="normal"
+                                validators={['required', 'minStringLength:3']}
+                                errorMessages={['this field is required', 'Слишком короткое значение ']}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField
+                            <TextValidator
                                 id="standard-surname"
                                 label="Фамилия"
                                 fullWidth
+                                name='surname'
                                 className={classes.textField}
                                 value={this.state.surname}
                                 onChange={this.handleChange('surname')}
                                 margin="normal"
+                                validators={['required', 'minStringLength:3']}
+                                errorMessages={['this field is required', 'Слишком короткое значение ']}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField
-                                id="standard-surname"
+                            <TextValidator
+                                id="standard-mail"
                                 label="Почта"
+                                name='email'
                                 fullWidth
                                 className={classes.textField}
                                 value={this.state.mail}
-                                onChange={this.handleChange('surname')}
+                                onChange={this.handleChange('mail')}
+                                validators={['required', 'isEmail']}
+                                errorMessages={['this field is required', 'email is not valid']}
                                 margin="normal"
                             />
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField
-                                id="standard-surname"
-                                label="Номер телефона"
-                                fullWidth
-                                className={classes.textField}
+                            <InputMask
+                                mask="+7 999 999 99 99"
                                 value={this.state.phone}
                                 onChange={this.handleChange('phone')}
-                                margin="normal"
-                            />
+                                required
+                            >
+                                {() => <TextValidator id="standard-surname"
+                                                  label="Номер телефона"
+                                                  name="phone"
+                                                  fullWidth
+                                                  className={classes.textField}
+                                                  margin="normal"
+                                                  />}
+                            </InputMask>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField
+                            <TextValidator
                                 id="standard-surname"
                                 label="Дополнительная информация"
                                 fullWidth
                                 multiline={true}
                                 rows={6}
+                                name='text'
                                 rowsMax={10}
                                 className={classes.textField}
                                 value={this.state.text}
@@ -103,10 +136,10 @@ class TextFields extends React.Component {
                             />
                         </Grid>
                     </Grid>
-                    <Button variant="contained" color="primary" size='large' className={classes.button}>
+                    <Button type='submit' variant="contained" color="primary" size='large' className={classes.button}>
                         Отправить
                     </Button>
-                </form>
+            </ValidatorForm>
         );
     }
 }
